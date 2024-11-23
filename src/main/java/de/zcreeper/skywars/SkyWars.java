5f3ef6b0646b5f722a19416bd95e1bfabe58cf92
Michael <michaeljens09@gmail.com>
2024-11-23 21:35:17 +0100

    Added base

---
+++ src/main/java/de/zcreeper/skywars/SkyWars.java
@@ -0,0 +1,17 @@
+package de.zcreeper.skywars;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SkyWars extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
